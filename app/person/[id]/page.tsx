fc31189bf03ae5cb9f089810a5812b809d75c1c7
inwesol <inwesol.coach@gmail.com>
2025-05-24 22:42:01 +0530

    fix: typescript errors fixed.

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,6 +1,6 @@
-import PersonDetailsPage from "@/app/PersonDetailsPage/page";
+import PersonDetailsPage from '@/app/PersonDetailsPage/page'
 
 // This is a dynamic route page component that receives params from the URL
-export default function PersonPage({ params }) {
-  return <PersonDetailsPage params={params} />;
+export default function PersonPage({ params }: { params: { id: string } }) {
+  return <PersonDetailsPage params={params} />
 }
